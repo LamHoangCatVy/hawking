2ec23e79762cff4a972249aebf8fe535c1932ff7
Tsukuro <vongola392112@gmail.com>
2024-02-15 21:08:39 +0700

    fix:fix export User.js

---
+++ controller/authController.js
@@ -0,0 +1,53 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+const register = async (req, res) => {
+    const { name, email, password } = req.body;
+    if(!email || !password || !name){
+        return res.status(400).json({message: "Please provide all the fields"})
+    }
+    const userAlreadyExists = await User.findOne({email});
+
+    if(userAlreadyExists){
+        return res.status(400).json({message: "Email already exists"})
+    }
+    const passwordHash = await bcrypt.hash(password, 12);
+    const user = await User.create({name, email, password: passwordHash});
+    const token = user.createJWT();
+    
+    res.status(201).json({
+        user: {
+            email: user.email,
+            name: user.name,
+        },
+        token
+    })
+}
+const login = async (req, res) => {
+    const {email, password } = req.body;
+    if(!email ||  !password){
+        return res.status(400).json({message: "Please provide all the fields"})
+    }
+    const user = await User.findOne({email}).select("+password");
+    console.log(user);
+    if(!user){
+        return res.status(400).json({message: "Invalid credentials"});
+    }
+    const isCorrect = await user.correctPassword(password);
+    if(!isCorrect){
+        return res.status(400).json({message: "Wrong password"});
+    }
+    const token = user.createJWT();
+    user.password = undefined;
+    res.status(200).json({
+        user,
+        token
+    })
+
+}
+
+const logout=async(req,res)=>{
+    res.cookie('jwt','',{maxAge:1});
+    res.redirect('/');
+}
+
+export {register , login, logout};
