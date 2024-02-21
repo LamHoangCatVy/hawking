005214585cceb5c03c77933fa0120c3c4246d3cb
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2024-02-21 14:19:30 +0700

    redirect unverified user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ type Props = {};
 
 const Navbar = async (props: Props) => {
   const session = await getAuthSession()
-  console.log(session)
   return (
     <nav className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl">
